86beb373647cb4392999654dc729d0d82fa8a851
Brightcells <brightcells@gmail.com>
2018-06-11 14:57:42 +0800

    :sparkles: Add new api set_cookie

--- django_uniapi/urls.py
+++ django_uniapi/urls.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-
 from django_uniapi import views as uni_views
 
 
@@ -9,4 +8,8 @@ urlpatterns = [
     url(r'^status$', uni_views.server_status, name='server_status'),
     url(r'^time$', uni_views.server_time, name='server_time'),
     url(r'^error$', uni_views.raise_error, name='raise_error'),
+]
+
+urlpatterns += [
+    url(r'^set_cookie$', uni_views.set_cookie, name='set_cookie'),
 ]
--- django_uniapi/views.py
+++ django_uniapi/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django_response import response
 from TimeConvert import TimeConvert as tc
 
@@ -19,3 +20,25 @@ def raise_error(request):
     # Raise ZeroDivisionError
     zero_division_error = 1 / 0
     return response()
+
+
+def set_cookie(request):
+    cookie_key = request.GET.get('k', '')
+    cookie_value = request.GET.get('v', '')
+
+    signed_cookie = bool(request.GET.get('s', 1))
+
+    max_age = hasattr(settings, 'DJANGO_WE_COOKIE_MAX_AGE') and getattr(settings, 'DJANGO_WE_COOKIE_MAX_AGE') or 30 * 24 * 60 * 60  # 30d
+    cookie_salt = hasattr(settings, 'DJANGO_WE_COOKIE_SALT') and getattr(settings, 'DJANGO_WE_COOKIE_SALT') or 'djwe'  # Salt for ``set_signed_cookie``
+
+    resp = response()
+
+    if signed_cookie:
+        resp.set_signed_cookie(cookie_key, cookie_value, salt=cookie_salt, **{
+            'max_age': max_age,
+            'httponly': True,
+        })
+    else:
+        response.set_cookie(cookie_key, cookie_value, max_age=max_age)
+
+    return resp
